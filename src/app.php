67123ba8f5628ddcf1e66573864cffd0bae19993
John Johnson <jsofteng@gmail.com>
2018-03-10 23:07:50 -0500

    fixed security token issue

--- src/app.php
+++ src/app.php
@@ -18,11 +18,20 @@ $app->get('/analytics/hashtag/{context}',
     function(Request $request, Response $response, array $args){
       //query db
       $context = $args['context'];
+      $nowDate = time();
+      $yesterdayDate = strtotime('-1 day', $nowDate);
+      $params = [
+        'TableName' => 'frameworks',
+        'Limit' => 60,
+        'KeyConditionExpression' => 'framework = :v_hash and unix_time between :v_range1 and :v_range2 ',
+        'ExpressionAttributeValues' => array(
+            ':v_hash' => array('S' => $context),
+            ':v_range1' => array('N' => $yesterdayDate),
+            ':v_range2' => array('N' => $nowDate)
+        )
+      ];
       try{
-        $result = $GLOBALS['g_client']->scan(array(
-          'TableName' => $context,
-          'Limit' => 5
-        ));
+        $result = $GLOBALS['g_client']->query($params);
       } catch(Exception $e){
           if (strcmp($e->getMessage(),"Requested resource not found") == 0){
             return "Spin up another listener for " . $context . "!";
