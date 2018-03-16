cd75de9d9911cc5995ad8543616401241385ff54
John Johnson <jsofteng@gmail.com>
2018-03-16 13:32:47 -0400

    added http header

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
 <?php
+
+header("Access-Control-Allow-Origin: http://s3storage-twingie-testbucket.s3-website.us-east-2.amazonaws.com");
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require __DIR__ . '/../src/app.php';
