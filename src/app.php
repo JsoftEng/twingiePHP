1bf01c9a3f8c1f43f56b01bdb3e4c63592b06bde
John Johnson <jsofteng@gmail.com>
2018-04-14 10:59:13 -0400

    modified routing

--- src/app.php
+++ src/app.php
@@ -5,6 +5,7 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
   use Aws\Common\Aws;
+  use Aws\DynamoDb\Marshaler;
 
   // Setup Application
   $app = new \Slim\App;
@@ -12,6 +13,7 @@
   // Setup Database
   $g_aws = Aws::factory(__DIR__ . '/../config.php');
   $g_client = $g_aws->get('DynamoDb');
+  $g_marshaler = new Marshaler;
 
   // Enable CORS
   $app->options('/{routes:.+}', function ($request, $response, $args) {
@@ -39,21 +41,13 @@
       function(Request $request, Response $response, array $args){
         $state = $request->getQueryParam('state');
 
-        //set up parametrs for db query
-        $params = [
-          'TableName' => 'twingieStates',
-          'KeyConditionExpression' => 'stateABV = :v_hash',
-          'ExpressionAttributeValues' => array(
-              ':v_hash' => array('S' => $state),
-          )
-        ];
-
-        //query db
         try{
-          $result = $GLOBALS['g_client']->query($params);
-        } catch(Exception $e){
-              return "Error: " . $e->getMessage();
+          $result = queryState($state);
+        }catch(Exception $e){
+          return "Error: " . $e->getMessage();
         }
+
+        //echo $GLOBALS['g_marshaler']->marshalJson($result);
 
         return $response
           -> withStatus(200)
@@ -64,27 +58,105 @@
   $app->get('/analytics/senator',
       function(Request $request, Response $response, array $args){
         $twitterID = $request->getQueryParam('twitterid');
+        $activity_by_interval = 'activity_by_interval';
 
-        //set up parametrs for db query
-        $params = [
-          'TableName' => 'twingieSenators',
-          'KeyConditionExpression' => 'senatorID = :v_hash',
-          'ExpressionAttributeValues' => array(
-              ':v_hash' => array('S' => $twitterID),
-          )
-        ];
-
-        //query db
         try{
-          $result = $GLOBALS['g_client']->query($params);
-        } catch(Exception $e){
-              return "Error: " . $e->getMessage();
+          if (checkInterval()){
+            $result = getAnalysis($twitterID);
+
+            //$result = querySenator($twitterID);
+          }else{
+            $analysisResult = getAnalysis($twitterID);
+            depositAnalysis($analysisResult, $twitterID);
+            $result = querySenator($twitterID);
+          }
+        }catch(Exception $e){
+          return "Error: " . $e->getMessage();
         }
 
         return $response
           -> withStatus(200)
-          -> withJson($result['Items']);
+          -> withJson($result);
+          //-> withJson($result->$activity_by_interval);
       }
   );
+
+  function queryState($state){
+    //set up parametrs for querying state db
+    $params = [
+      'TableName' => 'twingieStates',
+      'KeyConditionExpression' => 'stateABV = :v_hash',
+      'ExpressionAttributeValues' => array(
+          ':v_hash' => array('S' => $state),
+      )
+    ];
+
+    //query db
+    $result = $GLOBALS['g_client']->query($params);
+    return $result;
+  }
+
+  function querySenator($twitterID){
+    //set up parametrs for querying senator db
+    $params = [
+      'TableName' => 'twingieSenators',
+      'KeyConditionExpression' => 'senatorID = :v_hash',
+      'ExpressionAttributeValues' => array(
+          ':v_hash' => array('S' => $twitterID),
+      )
+    ];
+
+    $result = $GLOBALS['g_client']->query($params);
+    return $result;
+  }
+
+  function depositAnalysis($analysis, $twitterID){
+    $activity_by_interval = 'activity_by_interval';
+    $most_controversial_tweet = 'most_controversial_tweet';
+    $most_popular_tweet = 'most_popular_tweet';
+    $related_hashtag = '';
+    $related_user = '';
+    $sentiment = '';
+
+    $params = [
+      'TableName' => 'twingieSenators',
+      'Item' => array(
+        $twitterID => $GLOBALS['g_marshaler']->marshalItem($analysis['activity_by_interval'])
+      )
+    ];
+
+    //var_dump($GLOBALS['g_marshaler']->marshalItem($analysis['activity_by_interval']));
+
+    //var_dump($GLOBALS['g_marshaler']->marshalItem($analysis));
+    //$GLOBALS['g_client']->putItem($params);
+  }
+
+  function checkInterval(){
+    $isWithinInterval = false;
+    return $isWithinInterval;
+  }
+
+  function getAnalysis($senator){
+    $activity_by_interval = 'activity_by_interval';
+    $most_controversial_tweet = 'most_controversial_tweet';
+    $most_popular_tweet = 'most_popular_tweet';
+    $related_hashtag = 'related_hashtag';
+    $related_user = 'related_user';
+    $sentiment = 'sentiment';
+
+    $json = file_get_contents('http://twingiems-dev.us-east-1.elasticbeanstalk.com/?user='.$senator);
+    $decodedJson = json_decode($json);
+
+    $result = array(
+      'activity_by_interval' => $decodedJson->$activity_by_interval,
+      'most_controversial_tweet' => $decodedJson->$most_controversial_tweet,
+      'most_popular_tweet' => $decodedJson->$most_popular_tweet,
+      'related_hashtag' => $decodedJson->$related_hashtag,
+      'related_user' => $decodedJson->$related_user,
+      'sentiment' => $decodedJson->$sentiment
+    );
+
+    return $result;
+  }
 
   $app->run();
