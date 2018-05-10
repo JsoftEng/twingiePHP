54a0314b26d0ae6e7ce588e462559894b6cfcde4
John Johnson <jsofteng@gmail.com>
2018-05-09 23:53:14 -0400

    modified cors header

--- src/app.php
+++ src/app.php
@@ -38,7 +38,7 @@
   $app->add(function ($req, $res, $next) {
       $response = $next($req, $res);
       return $response
-              ->withHeader('Access-Control-Allow-Origin', 'http://s3storage-twingie-testbucket.s3-website.us-east-2.amazonaws.com')
+              ->withHeader('Access-Control-Allow-Origin','*')
               ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
               ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   });
